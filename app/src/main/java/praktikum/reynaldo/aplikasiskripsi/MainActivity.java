35ee3164dafd72f27c2d81dd9a7ce071f7c4f18b
reynaldokrisdiyanto <45395887+reynaldokrisdiyanto@users.noreply.github.com>
2019-12-20 23:12:46 +0700

    Layout Aplikasi Skripsi
    Layout Aplikasi Skripsi

---
+++ app/src/main/java/praktikum/reynaldo/aplikasiskripsi/MainActivity.java
@@ -0,0 +1,28 @@
+package praktikum.reynaldo.aplikasiskripsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCari;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCari = findViewById(R.id.btnCari);
+        btnCari.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+                finish();
+            }
+        });
+    }
+}
